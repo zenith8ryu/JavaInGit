f7632fb51a64a08ecb34af4d9663bfc9b6f89cb5
zenith7ryu <yumemiya8ryu@gmail.com>
2018-02-19 21:58:45 +0900

    Interview problem 2018-02-19

---
+++ src/interview/SunBridge.java
@@ -0,0 +1,24 @@
+package interview;
+
+public class SunBridge {
+    public static void main(String[] args) {
+        hoge();
+    }
+
+    private static void hoge() {
+        for (int i = 1; i <= 100; i++) {
+            String str = String.valueOf(i);
+            if (str.contains("3")) {
+                System.out.println("hoge");
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.println("hoge");
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
