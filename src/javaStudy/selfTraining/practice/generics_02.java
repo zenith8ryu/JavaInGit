cd37a29b3d46362325368075ced441d707819d7a
yumemiya8ryu <30719838+yumemiya8ryu@users.noreply.github.com>
2018-03-20 18:59:47 +0900

    slightly modify

--- src/javaStudy/selfTraining/practice/generics_02.java
+++ src/javaStudy/selfTraining/practice/generics_02.java
@@ -2,15 +2,21 @@ package javaStudy.selfTraining.practice;
 
 public class generics_02 {
     public static void main(String[] args) {
+        excute();
+    }
+
+    private static void excute() {
         // Implicit call a generics type method -> bad
         System.out.println((new Fan()).mtd_01(new Integer(10)));
         // explicit call a generics type method -> good
         System.out.println((new Fan()).<Integer>mtd_01(new Integer(10)));
     }
-}
 
-class Fan {
-    public <T> T mtd_01(T a) {
-        return a;
+    public class Fan {
+        private final
+
+        private <T> T mtd_01(T a) {
+            return a;
+        }
     }
 }
