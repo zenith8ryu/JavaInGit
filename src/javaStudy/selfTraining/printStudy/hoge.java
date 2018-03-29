786a98e845190e61f6a0fdfdead9d7d87203c31d
yumemiya8ryu <30719838+yumemiya8ryu@users.noreply.github.com>
2018-03-29 16:31:33 +0900

    slightly modify

---
+++ src/javaStudy/selfTraining/printStudy/hoge.java
@@ -0,0 +1,11 @@
+package javaStudy.selfTraining.printStudy;
+
+public class hoge {
+    public static void main(String[] args) {
+        printer();
+    }
+
+    public static void printer() {
+        
+    }
+}
