fd98f5c6d1e65a0b1ceec62b76cd04175372aeed
zenith7ryu <yumemiya8ryu@gmail.com>
2018-05-25 09:03:47 +0900

    Reasoning Q, rev20180525_0903

--- src/challenge/NerilTest.java
+++ src/challenge/NerilTest.java
@@ -8,43 +8,44 @@ public class NerilTest {
 
     public static void main(String[] args) {
         int start = Integer.parseInt("0000000000", 4);
-        int end = Integer.parseInt("3333333333", 4);
+//        int end = Integer.parseInt("3333333333", 4);
+        int end = Integer.parseInt("0000000033", 4);
 
 
         for (int i = start; i <= end; i++) {
             String ansStr = String.format("%10s", Integer.toString(i, 4)).replaceAll(" ", "0");
             HashMap<Integer, String> ansList = Tools.ansFulfill(ansStr);
 
-            if (!question01.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question02.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question03.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question04.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question05.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question06.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question07.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question08.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question09.ansCheck(ansList)) {
-                continue;
-            }
-            if (!question10.ansCheck(ansList)) {
-                continue;
-            }
+//            if (!question01.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question02.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question03.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question04.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question05.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question06.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question07.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question08.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question09.ansCheck(ansList)) {
+//                continue;
+//            }
+//            if (!question10.ansCheck(ansList)) {
+//                continue;
+//            }
 
 
             System.out.println("Round" + i);
--- src/challenge/test.java
+++ src/challenge/test.java
@@ -2,12 +2,10 @@ package challenge;
 
 public class test {
     public static void main(String[] args) {
-        int num = Integer.parseInt("0000000000", 4);
-        String str = Integer.toString(num);
-        String modStr = String.format("%10s",str.replaceAll(" ", "0"));
-        String modStr2 = String.format("%10s", Integer.toString(num, 4)).replaceAll(" ", "0");
+        int i = Integer.parseInt("3333313333", 4);
+        System.out.println(String.format("%10s", Integer.toString(i+2, 4)).replaceAll(" ", "0"));
+//        System.out.println(String.format("%10s", Integer.toString(i+2, 4)));
+        System.out.println(String.format("%10d", Integer.toString(i+2, 4)));
 
-
-        System.out.println(modStr);
     }
 }
